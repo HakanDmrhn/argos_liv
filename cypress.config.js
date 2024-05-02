8277d4d9d6eeaab363425152fb0b0624b75ff6e8
Hilal_B_P <hilal.posluoglu@hotmail.com>
2024-05-02 15:48:39 +0200

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -5,14 +5,11 @@ module.exports = defineConfig({
   defaultCommandTimeout: 30000,
   responseTimeout: 60000,
   chromeWebSecurity: false,
-  redirectionLimit: 50,
-  userAgent: 'testing_agent',
+  redirectionLimit: 100,
+  userAgent: 'testing_agent_visual',
   env: {
     AUTH_USER: 'staging',
-    AUTH_PASS: 'Mimfi34Voxuv',
-    js_files_url: '/js_minify/*.min.js',
-    js_files_hostname: 'www.plissee-experte.de',
-    catch_JS_Error: true,
+    AUTH_PASS: 'staging_pwd',
   },
   // setupNodeEvents can also be defined in "component"
   e2e: {
@@ -22,7 +19,7 @@ module.exports = defineConfig({
         // Enable upload to Argos only when it runs on CI.
         uploadToArgos: !!process.env.CI,
         // Set your Argos token (required only if you don't use GitHub Actions).
-        // token: "7f5b38da220eeccf5d8c12dde30696671e59c012",
+        token: "e8c2ba64bbf6ff84695a8a4de8c55e770a635dc5",
       });
 
       // include any other plugin code...
